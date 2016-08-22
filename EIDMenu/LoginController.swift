68e2a8f5292f024bc9f23d6c1c6932ac821f20bc
Administrador <admin@Administradors-MacBook-Pro.local>
2016-08-22 20:26:06 -0300

    Commit com tudo da pasta

--- EIDMenu/LoginController.swift
+++ EIDMenu/LoginController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController {
+    @IBOutlet weak var btn_entrar: UIButton!
+    @IBOutlet weak var txt_login: UITextField!
+    @IBOutlet weak var txt_senha: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        let backgroundImage = UIImageView(frame: UIScreen.mainScreen().bounds)
+        backgroundImage.image = UIImage(named: "back_dagrede.jpg")
+        self.view.insertSubview(backgroundImage, atIndex: 0)
+        
+        txt_login.layer.cornerRadius = 0
+        txt_senha.layer.cornerRadius = 0
+        
+        btn_entrar.backgroundColor = UIColor.clearColor()
+        btn_entrar.layer.cornerRadius = 0
+        btn_entrar.layer.borderWidth = 1
+        btn_entrar.layer.borderColor = UIColor.whiteColor().CGColor
         // Do any additional setup after loading the view, typically from a nib.
     }
 

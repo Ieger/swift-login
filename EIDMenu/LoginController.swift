ee8b22a00f2d84067b3206ef3240d2526dfd93c5
Administrador <admin@Administradors-MacBook-Pro.local>
2016-09-27 16:48:08 -0300

    ˜kkk˜

--- EIDMenu/LoginController.swift
+++ EIDMenu/LoginController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField;
 
 class LoginController: UIViewController {
     @IBOutlet weak var btn_entrar: UIButton!
@@ -18,24 +19,14 @@ class LoginController: UIViewController {
         
         navigationController?.setNavigationBarHidden(true, animated: false)
         
-        let backgroundImage = UIImageView(frame: UIScreen.mainScreen().bounds)
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "back_dagrede.jpg")
-        self.view.insertSubview(backgroundImage, atIndex: 0)
+        self.view.insertSubview(backgroundImage, at: 0)
         
-        txt_login.layer.cornerRadius = 0
-        txt_senha.layer.cornerRadius = 0
-        
-        let bottomLine = CALayer()
-        bottomLine.frame = CGRectMake(0.0, txt_senha.frame.height - 1, txt_senha.frame.width, 1.0)
-        bottomLine.backgroundColor = UIColor.whiteColor().CGColor
-        txt_login.borderStyle = UITextBorderStyle.None
-        txt_login.layer.addSublayer(bottomLine)
-     
-        
-        btn_entrar.backgroundColor = UIColor.clearColor()
+        btn_entrar.backgroundColor = UIColor.clear
         btn_entrar.layer.cornerRadius = 0
         btn_entrar.layer.borderWidth = 1
-        btn_entrar.layer.borderColor = UIColor.whiteColor().CGColor
+        btn_entrar.layer.borderColor = UIColor.white.cgColor
         // Do any additional setup after loading the view, typically from a nib.
     }
 
